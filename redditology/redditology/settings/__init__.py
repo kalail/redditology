f16d49c64ee755139a7faa6a2db9ec25e481fb87
Kalail <kalail@ubuntu.(none)>
2013-03-25 20:14:20 -0500

    Fix settings.

--- redditology/redditology/settings/__init__.py
+++ redditology/redditology/settings/__init__.py
@@ -38,16 +38,6 @@ TEMPLATE_LOADERS = (
 	'django.template.loaders.app_directories.Loader'
 )
 
-MIDDLEWARE_CLASSES = (
-	'django.middleware.common.CommonMiddleware',
-	'django.contrib.sessions.middleware.SessionMiddleware',
-	'django.middleware.csrf.CsrfViewMiddleware',
-	'django.contrib.auth.middleware.AuthenticationMiddleware',
-	'django.contrib.messages.middleware.MessageMiddleware',
-	'django.middleware.clickjacking.XFrameOptionsMiddleware',
-	'debug_toolbar.middleware.DebugToolbarMiddleware',
-)
-
 ROOT_URLCONF = 'redditology.urls'
 
 # WSGI server for debug server.
