03aa8a3e48667aab80003f01fe5d979fc5e494e0
Kalail <kalail@ubuntu.(none)>
2013-03-26 04:33:29 -0500

    Add proxy object to fix requests bug.

--- redditology/fetcher/tasks.py
+++ redditology/fetcher/tasks.py
@@ -10,6 +10,9 @@ from .models import Fetch
 
 logger = get_task_logger(__name__)
 
+class post_object(object):
+	pass
+
 
 @task
 def fetch_posts():
@@ -35,14 +38,29 @@ def fetch_posts():
 	# Exit if failed
 	if not fetch.successful:
 		return
-	# Send save post tasks
+	# import pdb; pdb.set_trace()
 	logger.info('Dispatching save tasks')
 	for idx, post in enumerate(posts):
 		# Correct rank offset
 		rank = idx + 1
-		# Create parsed post
-		post_cache_key = '%s - %s' % (post, post.id)
-		cache.set(post_cache_key, post)
+
+		# Bug in requests prevents pickling post. Create p as proxy object.
+		p = post_object()
+		p.id = post.id
+		p.title = post.title
+		p.author = post.author.name
+		p.url = post.url
+		p.domain = post.domain
+		p.over_18 = post.over_18
+		p.subreddit = post.subreddit.display_name
+		p.num_comments = post.num_comments
+		p.created_utc = post.created_utc
+		p.ups = post.ups
+		p.downs = post.downs
+		
+		post_cache_key = 'fetched_post_%s' % (p.id)
+		cache.set(post_cache_key, p)
+
 		save_fetched_post.delay(post_cache_key, rank,  fetch.id)
 		logger.info('Sent rank %s post task' % rank)
 
@@ -77,8 +95,7 @@ def save_fetched_post(post_cache_key, rank, fetch_id):
 	p.domain = post.domain
 	p.over_18 = post.over_18
 	p.subreddit = post.subreddit
-	p.domain = post.domain
-	p.created_on_reddit = datetime.utcfromtimestamp(p.created_on).replace(tzinfo=pytz.UTC)
+	p.created_on_reddit = datetime.utcfromtimestamp(post.created_utc).replace(tzinfo=pytz.UTC)
 	p.save()
 
 	fetch = Fetch.objects.get(id=fetch_id)
